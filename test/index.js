c5b759f10be69e272ea493ebc0a460ad313dab85
OFFICE\kontanin <kontanin@dextragroup.com>
2022-06-09 10:09:51 +0700

    no message

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ module.exports = async function (context, req) {
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully edit1234567890-."
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully edit1234567890-TESTfor P'DON."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
         context.log('`1234567890');
     context.res = {
